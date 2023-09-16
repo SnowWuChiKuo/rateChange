8567febc2344f20d2b31cb2c6df4eeeaee4e7cd3
SnowWuChiKuo <https://github.com/SnowWuChiKuo>
2023-09-17 00:37:29 +0800

    feat: add exchange rate method

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const data = require('../../models/data')
 
-router.get('/', (req, res) => {
-  console.log(req.query) // 找取前端資料
-  let amount = 0
-  res.render('index', { amount })
+router.get('/',  async(req, res) => {
+  try {
+    const coinData = data.currencies
+    const source = req.query.source
+    const target = req.query.target
+    let amount = req.query.amount
+    const rates = {
+      TWD: coinData.TWD,
+      JPY: coinData.JPY,
+      USD: coinData.USD,
+    }
+    if (rates[source] && rates[source][target]) {
+      let amountChange = amount.split(',').join("")
+      amount = Math.round(amountChange * rates[source][target] * 100) / 100
+    }
+    res.render('index', { amount })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: 'Server Error!'})
+  }
 })
 
 module.exports = router
