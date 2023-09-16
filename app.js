cbf3f3ad2d9e87781be4a0692631a53769347685
SnowWuChiKuo <https://github.com/SnowWuChiKuo>
2023-09-16 17:06:13 +0800

    feat: add home page & models

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express')
-const app = express()
+const handlebars = require('express-handlebars')
+const routes = require('./routes')
 
+const app = express()
 // 建立 port 號碼
 const port = 3000
 
-// 使用 Express 傳送回應給使用者
-app.get('/', (req, res) => {
-  res.send(`This is my first Express Web App`)
-})
+//使用 樣板引擎(第一個參數是樣板引擎的名稱，第二個參數是放入此樣板引擎的相關設定)
+app.engine('hbs', handlebars({ defaultLayout: 'main', extname: 'hbs' }))
+//透過app.set告訴Express說要設定的 view engine 是 handlebars
+app.set('view engine', 'hbs') //若engine是handlebars是縮寫，此處的handlebars也是要縮寫
+
+
+app.use(routes)
 
 // 啟動並監聽伺服器
 app.listen(port, () => {
